2f2f93e668907c32843dbacba18d692cca3af411
unknown <dev.bogie@gmail.com>
2024-06-12 11:39:26 +0800

    first commit

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Isi konten Blog</h3>
+</x-layout>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Isi konten Contact</h3>
+</x-layout>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">Isi konten Home</h3>
+</x-layout>
