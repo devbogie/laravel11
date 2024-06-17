0e5c5171348e36bf59845e6321992cc11ac70b46
unknown <dev.bogie@gmail.com>
2024-06-17 18:38:43 +0800

    third commit

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -17,46 +17,12 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', [
         'title' => 'Blog',
-        'posts' => [
-            [
-                'id' => 1,
-                'slug' => 'judul-artikel-1',
-                'title' => 'Judul Artikel 1',
-                'author' => 'Bogie',
-                'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos eveniet, corporis dicta cum in officia neque natus non magni blanditiis quisquam facilis officiis itaque assumenda aut adipisci sapiente unde tempore?'
-            ],
-            [
-                'id' => 2,
-                'slug' => 'judul-artikel-2',
-                'title' => 'Judul Artikel 2',
-                'author' => 'Ganilaga',
-                'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat, nulla doloribus at itaque iste, sint non ipsam ipsum omnis ipsa laudantium ut maxime unde sit velit necessitatibus atque amet.'
-            ]
-        ]
+        'posts' => Post::all()
     ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul Artikel 1',
-            'author' => 'Bogie',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos eveniet, corporis dicta cum in officia neque natus non magni blanditiis quisquam facilis officiis itaque assumenda aut adipisci sapiente unde tempore?'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul Artikel 2',
-            'author' => 'Ganilaga',
-            'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat, nulla doloribus at itaque iste, sint non ipsam ipsum omnis ipsa laudantium ut maxime unde sit velit necessitatibus atque amet.'
-        ]
-    ];
-
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
 
     return view('post', [
         'title' => 'Single Post',
