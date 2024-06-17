a2c0ec47c60300a065b0a5adc6e7459cb60bcff9
unknown <dev.bogie@gmail.com>
2024-06-17 21:45:45 +0800

    4th commit

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,43 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    // fungsi menampilkan semua post
-    public static function all()
-    {
-        return
-            [
-                [
-                    'id' => 1,
-                    'slug' => 'judul-artikel-1',
-                    'title' => 'Judul Artikel 1',
-                    'author' => 'Bogie',
-                    'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos eveniet, corporis dicta cum in officia neque natus non magni blanditiis quisquam facilis officiis itaque assumenda aut adipisci sapiente unde tempore?'
-                ],
-                [
-                    'id' => 2,
-                    'slug' => 'judul-artikel-2',
-                    'title' => 'Judul Artikel 2',
-                    'author' => 'Ganilaga',
-                    'body' => 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat, nulla doloribus at itaque iste, sint non ipsam ipsum omnis ipsa laudantium ut maxime unde sit velit necessitatibus atque amet.'
-                ]
-            ];
-    }
-
-    // fungsi menampilkan post spesifik
-    public static function find($slug): array
-    {
-        $post =  Arr::first(static::all(), fn ($post) => $post['slug'] == $slug);
-
-        // jika post tidak ada
-        if (!$post) {
-            abort(404);
-        }
-
-        // jika post ada
-        return $post;
-    }
+    use HasFactory;
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
